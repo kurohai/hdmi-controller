77be184e7c4de779f2a11aac0ad4a769c39170a9
Evan Burt <evantburt@gmail.com>
2017-11-12 02:16:33 -0600

    fixed paths

--- build/install.sh
+++ build/install.sh
@@ -12,15 +12,15 @@ mkdir -p /etc/kurohai
 cp -vr $PWD/build/* /etc/kurohai/
 
 
+chmod 700 /etc/kurohai/ssh
 chmod 600 /etc/kurohai/ssh/id_rsa
 chmod 600 /etc/kurohai/ssh/id_rsa.pub
-chmod 700 /etc/kurohai/ssh
 
 chown root:root /etc/kurohai/ssh
 chown root:root /etc/kurohai/ssh/id_rsa
 chown root:root /etc/kurohai/ssh/id_rsa.pub
 
-cp $PWD/build/tunnels.d/*.sh /etc/kurohai/tunnels.d/
-cp $PWD/build/tunnels.d/kurohai-tunnels.service /etc/systemd/system/kurohai-tunnels.service
+cp $PWD/build/*.sh /etc/kurohai/tunnels.d/
+cp $PWD/build/kurohai-tunnels.service /etc/systemd/system/kurohai-tunnels.service
 systemctl daemon-reload
 systemctl start kurohai-tunnels.service

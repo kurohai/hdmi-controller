8f5e47795188dcd9c075dc00c7f59a73a16dea5d
Evan Burt <evantburt@gmail.com>
2017-11-15 09:29:29 -0600

    small fixes

--- build/install.sh
+++ build/install.sh
@@ -12,6 +12,9 @@ mkdir -p /etc/kurohai/tunnels.d
 rsync -havt $PWD/build/start.sh /etc/kurohai/
 chmod +x /etc/kurohai/start.sh
 
+rsync -havt $PWD/build/start-app.sh /etc/kurohai/
+chmod +x /etc/kurohai/start-app.sh
+
 
 chmod 700 /etc/kurohai/ssh
 chmod 600 /etc/kurohai/ssh/id_rsa
@@ -24,5 +27,7 @@ chown root:root /etc/kurohai/ssh/id_rsa.pub
 
 rsync -havt $PWD/tunnels.d/ /etc/kurohai/tunnels.d/
 rsync -havt $PWD/build/kurohai-tunnels.service /etc/systemd/system/kurohai-tunnels.service
+rsync -havt $PWD/build/kurohai-hdmi-controller-app.service /etc/systemd/system/kurohai-hdmi-controller-app.service
 systemctl daemon-reload
 systemctl start kurohai-tunnels.service
+systemctl start kurohai-hdmi-controller-app.service
